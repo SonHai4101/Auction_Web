960b7ab9caa50c4701e71d2fdffd0c21efcd63cd
HaiSon <hhaihhai4101@gmail.com>
2021-10-26 23:32:46 +0700

    add password_hash

--- process-login.php
+++ process-login.php
@@ -3,19 +3,25 @@
 
         if(isset($_POST['btnlogin'])){
             $username = $_POST['txtUsername'];
-            $password = $_POST['txtPassword'];
+            $password_raw = $_POST['txtPassword'];
 
             $conn = mysqli_connect('localhost','root','','auction');
             if(!$conn){
                 die("Không thể kết nối");
             }
 
-            $sql = "SELECT * FROM dbo_users WHERE username='$username' AND userpassword ='$password'";
+            $sql = "SELECT * FROM dbo_users WHERE username='$username'";
             $result = mysqli_query($conn,$sql);
 
             if(mysqli_num_rows($result) > 0){
-                $_SESSION['loginOK'] = $username;
-                header("Location: user/index.php");
+                $row = mysqli_fetch_assoc($result);
+                $password_hash = $row['userpassword'];
+                echo $password_hash;
+                if(password_verify($password_raw,$password_hash)){
+                     header('Location: user/index.php');
+                }else{
+                    echo 'Mật khẩu không khớp';
+                }
             }else{
                 header("Location: login.php");
             }
